23a11ae74a0dd3b1a9ec93126371758538437adc
k1berg <tarn.aler@gmail.com>
2023-05-10 00:40:07 +0600

    answer for task 'pick'

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,14 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
+  let newArr = [];
+  Object.entries(obj).forEach((item) => (
+    fields.forEach(function(field) {
+      if (item[0] == field) {
+        newArr.push(item);
+      }
+    })
+  ));
 
+  return Object.fromEntries(newArr);
 };
